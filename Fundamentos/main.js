f1bc962e3616e2a01f0809fac27073c86ad1ae9c
gumerzzzindo <aitor025@protonmail.com>
2024-05-28 20:21:05 +0200

    Añadimos os.uptime() /60 /60

--- Fundamentos/main.js
+++ Fundamentos/main.js
@@ -1,4 +1,6 @@
 const os = require('node:os')
+const { uptime } = require('node:process')
 console.log(globalThis)
 console.log('Sistema:', os.platform())
 console.log('Arquitectura:', os.arch())
+console.log('Tiempo de uso:',os.uptime() / 60 /60)
